5f13142833d2b0b9670ce33f0ffbd7c9409efdc4
Niels Rood <n.rood@tue.nl>
2020-10-22 16:46:40 +0200

    updated paths in script in order to match the file structure as presented in SharePoint (which is copied directly into the data and models folders of this repository as cloned on the raspberry pi)

--- src/us15_model_to_run_in_raspberry.py
+++ src/us15_model_to_run_in_raspberry.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 import keras
 import librosa
 import librosa.display
-%pylab inline
+#%pylab inline
 import os
 import pandas as pd
 import librosa
@@ -15,10 +15,10 @@ loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 #load weights into new model
-loaded_model.load_weights("saved_models/Emotion_Voice_Detection_Model.h5")
+loaded_model.load_weights("models/tone_cnn_happy_angry/saved_models/Emotion_Voice_Detection_Model.h5")
 print("Loaded model from disk")
 
-X, sample_rate = librosa.load('output10.wav', res_type='kaiser_fast',duration=2.5,sr=22050*2,offset=0.5)
+X, sample_rate = librosa.load('data/tone_cnn_happy_angry_dataset_sample_prediction_tracks/output10.wav', res_type='kaiser_fast',duration=2.5,sr=22050*2,offset=0.5)
 sample_rate = np.array(sample_rate)
 mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13),axis=0)
 featurelive = mfccs
@@ -45,7 +45,7 @@ liveabc = livepreds1.astype(int).flatten()
 #load lable encoding
 
 lb = LabelEncoder()
-lb.classes_ = numpy.load('encoder.npy',allow_pickle=True)
+lb.classes_ = numpy.load('models/tone_cnn_happy_angry/encoder.npy',allow_pickle=True)
 
 livepredictions = (lb.inverse_transform((liveabc)))
 livepredictions
