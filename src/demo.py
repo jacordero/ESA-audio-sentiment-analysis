b87e29ccb8dc35127565db319df07cd73ecc5563
Cordero Cruz <j.a.cordero.cruz@tue.nl>
2020-11-03 19:12:33 +0100

    tone-based detection model works for the demo

--- src/demo.py
+++ src/demo.py
@@ -2,6 +2,7 @@
 
 import time
 import keyboard 
+import keras
 
 import sounddevice as sd
 import numpy as np
@@ -37,32 +38,42 @@ seconds = 5
 output_filename = "../data/output_{:0>4}.wav"
 
 
-
-def load_audio_model():
-	return None
-
-
 def load_text_model():
 	return None
 
-def prepare_audio_for_tone_model(audio_array, fs):
+def prepare_audio_for_tone_model(audio_array, sample_rate):
 	
-	sample_rate = np.array(fs)
-	mfccs = np.mean(mfcc(y=audio_array, sr=np.array(sample_rate), n_mfcc=13), axis=0)
+	mfccs = np.mean(mfcc(y=audio_array, sr=np.array(sample_rate), n_mfcc=40).T, axis=0)
+	x = np.expand_dims(mfccs, axis=0)
+	x = np.expand_dims(x, axis=2)
+	return x
 
-	featurelive = mfccs
-	livedf2 = featurelive
-	livedf2= pd.DataFrame(data=livedf2)
-	livedf2 = livedf2.stack().to_frame().T
-	twodim= np.expand_dims(livedf2, axis=2)
-
-	return twodim
 
 def prepare_sentences_for_text_model(audio_array, fs):
 	return None
 
 
-def run():
+emotions_dict = {0: 'neutral',
+    			 1: 'calm',
+    			 2: 'happy',
+    			 3: 'sad',
+    			 4: 'angry',
+    			 5: 'fearful',
+    			 6: 'disgust',
+			     7: 'surprised'}
+
+
+def print_emotions(title, emotion_probabilities):
+	
+	sorted_probabilities = sorted(emotion_probabilities, key=lambda x: x[1])
+	
+	print("\n***************************************")
+	print(title)
+	for e, p in sorted_probabilities:
+		print("{}: {:.2f}".format(e, p))
+	print("***************************************\n")
+
+def run(audio_model_filename, text_model_filename):
 
 	pause = 10
 	counter = 1
@@ -70,9 +81,9 @@ def run():
 	with open("ascii_astronaut.txt") as f:
 		print(f.read())
 
-	print("Loading models")
+	print("\n\n\nLoading models")
 
-	audio_model = load_audio_model()
+	audio_model = keras.models.load_model(audio_model_filename)
 	text_model = load_text_model()
 
 	print("Starting audio demo!")
@@ -95,21 +106,24 @@ def run():
 		sd.play(myrecording, fs)
 
 		print("Preprocessing audio for tone model")
-		audio_for_tone_model = prepare_audio_for_tone_model(np.squeeze(myrecording), fs)
+		preprocessed_audio = prepare_audio_for_tone_model(np.squeeze(myrecording), fs)
 
 		print("Preprocessing audio for text-based model")
 		sentences = prepare_sentences_for_text_model(myrecording, fs) 
 
 		## predictions happen here
-		audio_prediction = ""
+		audio_predictions = np.squeeze(audio_model.predict(preprocessed_audio))
+		audio_emotion_probabilities = [(emotions_dict[i], audio_predictions[i]) for i in range(len(audio_predictions))]
+
+		argmax_audio_prediction = np.argmax(np.squeeze(audio_predictions), axis=-1)
+		predicted_emotion = emotions_dict.get(argmax_audio_prediction, "Unknown")
 		text_prediction = ""
 
-
-		print("Audio prediction: {}".format(audio_prediction))
+		print_emotions("Audio prediction", audio_emotion_probabilities)
 		print("Text prediction: {}".format(text_prediction))
 
 
-		print(myrecording)
+		#print(myrecording)
 		counter += 1
 
 		print("Wait 10 seconds before recording new message")
@@ -118,4 +132,5 @@ def run():
 
 
 if __name__ == "__main__":
-	run()
+	audio_model_filename = "../models/tone_cnn_8_emotions/saved_models/Emotion_Voice_Detection_Model.h5"
+	run(audio_model_filename, "hoale")
