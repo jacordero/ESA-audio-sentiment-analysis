cb7f5950c956ee50135119db053493d25f1ef1dc
Cordero Cruz <j.a.cordero.cruz@tue.nl>
2020-11-03 21:48:55 +0100

    Both parameters are included for calling demo script

--- run_demo.sh
+++ run_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 script=src/demo.py
-audio_model="models/tone_cnn_8_emotions/saved_models/Emotion_Voice_Detection_Model.h5"
-text_model=
+audio_model="prod_models/tone_cnn_8_emotions/saved_models/Emotion_Voice_Detection_Model.h5"
+text_model="prod_models/emotion_text_model"
 
 python $script $audio_model $text_model
