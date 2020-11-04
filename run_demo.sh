556e63ee0892f524a4f900c31858ac0b8ce88e5c
Cordero Cruz <j.a.cordero.cruz@tue.nl>
2020-11-04 11:58:50 +0100

    live option is included in the bash script that executes the demo script

--- run_demo.sh
+++ run_demo.sh
@@ -3,5 +3,6 @@
 script=src/demo.py
 audio_model="prod_models/tone_cnn_8_emotions/saved_models/Emotion_Voice_Detection_Model.h5"
 text_model="prod_models/emotion_text_model"
+mode="live"
 
-python $script $audio_model $text_model
+python $script $audio_model $text_model $mode
