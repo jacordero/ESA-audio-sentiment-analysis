8851b4c40e58b418a2c2391dc5b55274ebd0cd8e
t.purevsuren <t.purevsuren@tue.nl>
2020-11-10 11:44:11 +0100

    Added parameters dictionary

--- src/retrain_tone_model.py
+++ src/retrain_tone_model.py
@@ -7,22 +7,20 @@ Copyright (C) Tu/e.
 
 """
 
+#!/usr/bin/env python
+
+#SBATCH --partition=mcs.gpu.q
+#SBATCH --output=results.out'
+
 import os
 import sys
 import time
 
-# tf.get_logger().setLevel('INFO')
 import librosa
 import numpy as np
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-
-#!/usr/bin/env python
-
-#SBATCH --partition=mcs.gpu.q
-#SBATCH --output=results.out
-
 
 def get_path(dir_name):
     """Find path of requested directory such as 'models' and 'data'.
@@ -55,23 +53,25 @@ def load_model(model_name):
         model_path = os.path.join(model_path, files[0])
         model = tf.keras.models.load_model(model_path)
         return model
-def save_model(model):
+
+def save_model(model, version):
     """Save the retrained tone model in models directory.
 
     Args:
       model: the retrained tone model.
+      version: the version to save retrained model.
 
     """
-    model_name = 'retrained_tone_model'
-    model_path = os.path.join(get_path('models'), model_name)
+    model_path = os.path.join(get_path('models'), '{name}_v.{version}'.format(name='retrained_tone_model', version=str(version)))
     model.save(model_path)
     print("The retrained model is saved at {}.".format(model_path))
 
-def prepare_datasets(dataset_name):
+def prepare_datasets(dataset_name, n_mfcc):
     """Load the dataset from data directory.
 
     Args:
       dataset_name: A name of dataset to be loaded.
+      n_mfcc: parameter for mfcc
 
     Returns:
       X as mfcc extracted feature of audio.
@@ -81,20 +81,17 @@ def prepare_datasets(dataset_name):
     lst = []
     data_path = os.path.join(get_path('data'), dataset_name)
 
-    # root is "tone_cnn_8_emotions_dataset" director
-    # dirs are subfolders such as "Actor_01" etc
-    # files are .wav files
     start_time = time.time()
     files = os.listdir(data_path)
     
-    for root, dirs, files in os.walk(data_path):
+    for root, _, files in os.walk(data_path):
         for audio_file in files:
             try:
-                # load librosa array, obtain mfcss, store the mfcss information in a new array
+                # load librosa array, obtain mfcss, store the mfcc information in a new array
                 X, sample_rate = librosa.load(os.path.join(root, audio_file), res_type='kaiser_fast')
 
                 # n_mfcc is setted 40 because the model is trained with 40 previously.
-                mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+                mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=n_mfcc).T, axis=0)
                 # convert labels from (1 ->8) to (0->7)
                 # predict script has emotion index (0 to 7)
                 audio_file = int(audio_file[7:8])-1
@@ -114,39 +111,50 @@ def prepare_datasets(dataset_name):
     print(y.shape)
     return X, y
 
-def retrain_model(model_name, dataset_name):
+def retrain_model(parameters):
     """Retrain the tone model with dataset.
 
     Args:
-      model_name: A name of model.
-      dataset_name: A name of dataset.
+      parameters: A set of parameters.
 
     """
-    model = load_model(model_name)
+    print(parameters)
+    model = load_model(parameters['model_name'])
     model.summary()
 
-    X, y = prepare_datasets(dataset_name)
-    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.33, random_state=42)
+    X, y = prepare_datasets(parameters['dataset_name'], parameters['n_mfcc'])
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=parameters['ratio_of_validation'], random_state=parameters['shuffle_random_seed'])
 
-    x_train_cnn = np.expand_dims(X_train, axis=2)
-    x_validation_cnn = np.expand_dims(X_validation, axis=2)
+    x_train_cnn = np.expand_dims(X_train, axis=parameters['axis'])
+    x_validation_cnn = np.expand_dims(X_validation, axis=parameters['axis'])
     print(x_train_cnn.shape)
     print(x_validation_cnn.shape)
 
-    opt = tf.keras.optimizers.Adam(learning_rate=0.001)
+    opt = tf.keras.optimizers.Adam(learning_rate=parameters['learning_rate'])
     model.compile(loss='sparse_categorical_crossentropy',
                   optimizer=opt,
                   metrics=['accuracy'])
 
     model.fit(x_train_cnn, y_train,
-              batch_size=8, epochs=1,
+              batch_size=parameters['batch_size'], epochs=parameters['epochs'],
               validation_data=(x_validation_cnn, y_validation))
-    save_model(model)
+    save_model(model, parameters['retrained_model_version'])
 
 if __name__ == "__main__":
     print("==== WELCOME to Sentiment Analysis for Astronauts System ====")
-    model_name = str(sys.argv[1])
-    dataset_name = str(sys.argv[2])
-    print("Model Name: ", model_name)
-    print("Dataset:", dataset_name)
-    retrain_model(model_name, dataset_name)
+    parameters = {
+      # resource related parameters
+      'model_name': sys.argv[1], 
+      'dataset_name': sys.argv[2], 
+      'retrained_model_version': sys.argv[3],
+      # dataset related parameters
+      'ratio_of_validation': 0.33,
+      'shuffle_random_seed': 42, 
+      'axis': 2, 
+      'n_mfcc': 40,
+      # retrain related parameters 
+      'batch_size': 8, 
+      'epochs': 1,
+      'learning_rate': 0.001,
+    }
+    retrain_model(parameters)
