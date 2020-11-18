35edd6bab50d3433cd39c86d2019e0583d8cfb13
Georgios Azis <g.azis@tue.nl>
2020-11-18 15:47:40 +0100

    Changes to yml and compute performance

--- tests/compute_models_performance.py
+++ tests/compute_models_performance.py
@@ -11,10 +11,10 @@ from librosa.feature import mfcc
 
 from context import tone_emotions
 from context import text_emotions
-from context import SequentialToneModelDataLoader
+from context import SequentialToneModelDataLoader, SiameseToneModelDataLoader
 
 
-def compute_text_model_performance(model_dir, test_data_dir):
+def compute_text_model_performance(model_dir, model_name, model_type, test_data_dir):
     """[summary]
 
     Args:
@@ -25,12 +25,33 @@ def compute_text_model_performance(model_dir, test_data_dir):
     print("compute_text_model_performance")    
 
     # load model
-    print("Load: {}".format(model_dir))
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path_to_dir =  os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir))
+    full_path_to_model =  os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir, model_name))
+    print("Full path to directory: {}".format(full_path_to_dir))
+    print("Full path to model: {}".format(full_path_to_model))
+
+    if "Sequential" in model_type:
+        print("Loading sequential text model...")
+        model = keras.models.load_model(full_path_to_dir)
+        data_loader = SequentialToneModelDataLoader()
+    elif "Siamese" in model_type:
+        print("Loading siamese text model...")
+        model = keras.models.load_model(full_path_to_model)
+        data_loader = SiameseToneModelDataLoader()
+    else:
+        raise ValueError("Value in configuration file {} is not supported".format('type'))
+
+    print(model.summary())
 
     # load data
     print("Load: {}".format(test_data_dir))
-
+ #   data_loader = SequentialToneModelDataLoader()
+ #   audios, labels = data_loader.load_test_data(test_data_dir)
+ #   predictions = np.squeeze(model.predict(audios))
+    
     # compute performance
+#    return compute_measures(predictions, labels)
     return None
 
 def compute_measures(predicted_values, truth_values):
@@ -40,9 +61,22 @@ def compute_measures(predicted_values, truth_values):
         predicted_values ([type]): [description]
         truth_values ([type]): [description]
     """    
+
+    # Confusion matrix for all categories
+
+
+    # Total accuracy of the model
+
+    # Perclass accuracy of the model
+
+    # F1 Score
+
+    # Confidence 
+
     return None
 
-def compute_tone_model_performance(model_name, test_data_dir):
+
+def compute_tone_model_performance(model_dir, model_name, model_type, test_data_dir):
     """[summary]
 
     Args:
@@ -53,25 +87,77 @@ def compute_tone_model_performance(model_name, test_data_dir):
     print("compute_tone_model_performance")    
     # load model
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    model_full_path =  os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_name))
-    print("Relative path: {}".format(model_name))
-    print("Full path: {}".format(model_full_path))
+    full_path_to_dir = os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir))
+    full_path_to_model =  os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir, model_name))
+    print("Full path to directory: {}".format(full_path_to_dir))
+    print("Full path to model: {}".format(full_path_to_model))
     
-    model = keras.models.load_model(model_full_path)
+    if "Sequential" in model_type:
+        print("Loading sequential tone model...")
+        model = keras.models.load_model(full_path_to_dir)
+        data_loader = SequentialToneModelDataLoader()
+    elif "Siamese" in model_type:
+        print("Loading siamese tone model...")
+        model = keras.models.load_model(full_path_to_model)
+        data_loader = SiameseToneModelDataLoader()
+    else:
+        raise ValueError("Value in configuration file {} is not supported".format('type'))
+
     print(model.summary())
     
     time.sleep(5)
     return 
-    
 
     # load data
     print("Load: {}".format(test_data_dir))
-    data_loader = SequentialToneModelDataLoader()
     audios, labels = data_loader.load_test_data(test_data_dir)
     predictions = np.squeeze(model.predict(audios))
     
     # compute performance
     return compute_measures(predictions, labels)
+
+
+def compute_model_performance(model_dir, model_name, model_type, test_data_dir):
+    """[summary]
+
+    Args:
+        model_dir ([type]): [description]
+        test_data_dir ([type]): [description]
+    """
+
+    print("compute_tone_model_performance")    
+    # load model
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path_to_dir = os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir))
+    full_path_to_model =  os.path.normpath(os.path.join(script_dir, "../prod_models/candidate", model_dir, model_name))
+    print("Full path to directory: {}".format(full_path_to_dir))
+    print("Full path to model: {}".format(full_path_to_model))
+    
+    if "Sequential" in model_type:
+        print("Loading sequential model...")
+        model = keras.models.load_model(full_path_to_dir)
+        data_loader = SequentialToneModelDataLoader()
+    elif "Siamese" in model_type:
+        print("Loading siamese model...")
+        model = keras.models.load_model(full_path_to_model)
+        data_loader = SiameseToneModelDataLoader()
+    else:
+        raise ValueError("Value in configuration file {} is not supported".format('type'))
+
+    print(model.summary())
+    
+    time.sleep(5)
+    return 
+
+    # load data
+    print("Load: {}".format(test_data_dir))
+    audios, labels = data_loader.load_test_data(test_data_dir)
+    predictions = np.squeeze(model.predict(audios))
+    
+    # compute performance
+    return compute_measures(predictions, labels)
+
+
 
 
 def save_performance_results(performance_results):
@@ -98,22 +184,22 @@ def generate_performance_summary(prod_config_file):
         prod_config_parameters = yaml.load(input_file, Loader=yaml.FullLoader)
 
     print(prod_config_parameters)
-
+    
     models = prod_config_parameters['models']
     test_data_dir = prod_config_parameters['test_data_dir']
     performance_results = {}
 
     for key in models:
-        if "tone" in key:
-            performance_results[key] = compute_tone_model_performance(models[key], test_data_dir)
-            #performance_results[key] = performance
-        elif "text" in key:
-            pass
-#            performance = compute_text_model_performance(models[key], test_data_dir)
+        if "tone_model" in key:
+            print("Computing performance of tone model...")
+            performance_results[key] = compute_model_performance(models[key]['dir'], models[key]['file'], models[key]['type'], test_data_dir)
+        elif "text_model" in key:
+            print("Computing performance of test model...")
+            performance_results[key] = compute_model_performance(models[key]['dir'], models[key]['file'], models[key]['type'], test_data_dir)
         else:
-            raise ValueError("Value in configuration file {} is not supported".format(key))
-
-    save_performance_results(performance_results)
+            raise ValueError("Value in configuration file {} is not supported".format('type'))
+###
+#    save_performance_results(performance_results)
 
 
 if __name__ == "__main__":
