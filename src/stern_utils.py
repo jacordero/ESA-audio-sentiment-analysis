48e8748f3b1e35ab556c31afa3461f32dbac4d8c
t.purevsuren <t.purevsuren@tue.nl>
2020-11-19 09:31:38 +0100

    Changed logging to logs and removed unneccessary functions

--- src/stern_utils.py
+++ src/stern_utils.py
@@ -12,7 +12,6 @@ import os
 import json 
 import time
 import pytz
-import resource
 import subprocess
 
 from datetime import datetime, date
@@ -53,7 +52,7 @@ def logging(data, logging_file_name):
       logging_file_name: A name of logging file.
 
     """  
-    folder_path = os.path.join(os.getcwd(), 'src/logging')
+    folder_path = os.path.join(os.getcwd(), 'src/logs')
     folder_name  = os.path.join(folder_path, logging_file_name + '_' + str(date.today()))
     # if directory does not exist , create
     if not os.path.exists(folder_path):
@@ -80,43 +79,3 @@ def logging(data, logging_file_name):
       with open(log_file_name, 'w+') as logging_file:
         json.dump([log_input], logging_file, indent=2)
         
-  
-def get_temperature():
-    """Measure the operating temperature of pi.
-
-       The maximum recommended operating temperature is 80 'C. 
-       If the operating temperature cross 85 'C, CPU starts throttling and reduces 
-       the clock to cool down the temperature. This will impact the performance.
-
-    Returns:
-      The current temperature of .
-
-    """
-    temp = os.popen("vcgencmd measure_temp").readline()
-    temp = temp.replace("temp=", "")
-    
-    if temp is "85'C":
-      print("WARNING: pi operating temperature is {}".format(temp) )
-
-    return (temp.replace("temp=", ""))
-
-def get_pi_resource_info(process):
-    """Retrieve resource usage information by a process.
-
-    Args:
-      process: A name of process.
-
-    """
-    usage = resource.getrusage(resource.RUSAGE_CHILDREN)
-
-    before = usage
-    print(subprocess.run(['python', process],  capture_output=True))
-    after = usage
-
-    print("==================================================================")
-    print("User CPU time = {} seconds.".format(after[0] - before[0]))
-    print("System CPU time = {} seconds.".format(after[1] - before[1]))
-    print("==================================================================")
-    print("MEMORY = {} kilobytes.".format(after[2] - before[2]))
-    print("==================================================================")
-    print("TEMPERATURE: {}".format(get_temperature()))
