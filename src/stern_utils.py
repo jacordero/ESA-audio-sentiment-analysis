e2224451617e895a914d6549149e8ead4a163718
Niels Rood <n.rood@tue.nl>
2020-11-19 13:49:42 +0100

    autopep8 on stern_utils

--- src/stern_utils.py
+++ src/stern_utils.py
@@ -13,7 +13,7 @@ import os
 import librosa
 import numpy as np
 import time
-import json 
+import json
 import time
 import pytz
 import subprocess
@@ -54,6 +54,7 @@ text_emotions = {0: 'neutral',
                  5: 'disgust',
                  6: 'surprised'}
 
+
 def save_wav_pcm(audio_array, output_audio_filename, audio_frequency):
     """Stores an audio signal as a pcm wav file
 
@@ -79,11 +80,13 @@ def get_path(dir_name):
       The full path of requested directory.
 
     """
-    path = os.path.join(os.path.dirname(os.path.normpath(os.getcwd())), dir_name)
-    if path: 
-      return path
+    path = os.path.join(os.path.dirname(
+        os.path.normpath(os.getcwd())), dir_name)
+    if path:
+        return path
     else:
-      raise Exception("NO DIRECTORY FOUND")
+        raise Exception("NO DIRECTORY FOUND")
+
 
 def get_time():
     """Get the current local time in the Netherlands.
@@ -96,6 +99,7 @@ def get_time():
     time_nl = datetime.now(tz_nl)
     return time_nl.strftime("%H")
 
+
 def logging(data, logging_file_name):
     """Write the data to logging file.
 
@@ -103,33 +107,35 @@ def logging(data, logging_file_name):
       data: list parameters for example [["13-11-20 09:53:12"], ["emotion"], ["emotion distribution"], ["text"]].
       logging_file_name: A name of logging file.
 
-    """  
+    """
     folder_path = os.path.join(os.getcwd(), 'src/logs')
-    folder_name  = os.path.join(folder_path, logging_file_name + '_' + str(date.today()))
+    folder_name = os.path.join(
+        folder_path, logging_file_name + '_' + str(date.today()))
     # if directory does not exist , create
     if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
+        os.makedirs(folder_path)
     if not os.path.exists(folder_name):
-      os.makedirs(folder_name)
+        os.makedirs(folder_name)
 
     log_input = {
-      'time': datetime.now().strftime('%d-%m-%y %H:%M:%S'), 
-      'text': data[0], 
-      'tone_emotion': data[1], 
-      'text_emotion': data[2]
-       }
+        'time': datetime.now().strftime('%d-%m-%y %H:%M:%S'),
+        'text': data[0],
+        'tone_emotion': data[1],
+        'text_emotion': data[2]
+    }
 
     log_file_name = os.path.join(folder_name, 'log_' + get_time() + '.json')
     if os.path.exists(log_file_name):
-      with open(log_file_name) as logging_file:
-        temp = json.load(logging_file)
-        temp.append(log_input)
-        with open(log_file_name, 'w') as logging_file:
-          json.dump(temp, logging_file, indent=2)
+        with open(log_file_name) as logging_file:
+            temp = json.load(logging_file)
+            temp.append(log_input)
+            with open(log_file_name, 'w') as logging_file:
+                json.dump(temp, logging_file, indent=2)
     else:
-      # create the file if not exist and write data to file.
-      with open(log_file_name, 'w+') as logging_file:
-        json.dump([log_input], logging_file, indent=2)
+        # create the file if not exist and write data to file.
+        with open(log_file_name, 'w+') as logging_file:
+            json.dump([log_input], logging_file, indent=2)
+
 
 class Utils:
     """
@@ -186,9 +192,9 @@ class Utils:
 
     @staticmethod
     def model_summary(model, x_testcnn, y_test):
-        if len(x_testcnn) >1:
+        if len(x_testcnn) > 1:
             predict_vector = model.predict(x_testcnn)
-            predictions = np.argmax(predict_vector,axis=1)
+            predictions = np.argmax(predict_vector, axis=1)
             new_y_test = y_test.astype(int)
             matrix = confusion_matrix(new_y_test, predictions)
             print(classification_report(new_y_test, predictions))
