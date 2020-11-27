8b358515ec3155d8056dac21218bbc8846f62ff0
Jorge Cordero <j.a.cordero.cruz@tue.nl>
2020-11-27 12:43:14 +0100

    Test datasets

--- tests/context.py
+++ tests/context.py
@@ -5,6 +5,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from src.utils import tone_emotions
 from src.utils import text_emotions
 from src.stern_utils import Utils
-from src.data_loader import SiameseToneModelDataLoader
-from src.data_loader import SequentialToneModelDataLoader
+from src.data_loader import SiameseDataLoader
+from src.data_loader import SequentialDataLoader
 from src.data_loader import TextModelDataLoader
