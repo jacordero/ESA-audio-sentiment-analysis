3347c83bfae11c9ed7b7aedf47caea3925793276
Georgios Azis <g.azis@tue.nl>
2020-11-30 19:01:45 +0100

    Test cases on schema validation

---
+++ tests/test_cases/Preprocessing/test_schema_verification.py
@@ -0,0 +1,52 @@
+# test_schema_verification.py
+import os
+import pytest
+import yamale
+
+
+def test_config_file_against_schema():
+    # Get schema
+    schema = yamale.make_schema('tests/test_cases/Preprocessing/configuration_schema.yml')
+
+    # Get configuration file
+    config_file  = yamale.make_data('tests/configuration.yml')
+
+    # Validate data against the schema. Throws a ValueError if data is invalid.
+    raised = True
+    try:
+        _ = yamale.validate(schema, config_file)
+        raised = False
+    finally:
+        assert raised != True
+
+
+def test_raspi_candidate_against_schema():
+    # Get schema
+    schema = yamale.make_schema('tests/test_cases/Preprocessing/raspi_candidate_config_schema.yml')
+
+    # Get configuration file
+    config_file  = yamale.make_data('src/raspi_candidate_config.yml')
+
+    # Validate data against the schema. Throws a ValueError if data is invalid.
+    raised = True
+    try:
+        _ = yamale.validate(schema, config_file)
+        raised = False
+    finally:
+        assert raised != True
+
+
+def test_raspi_deployment_against_schema():
+    # Get schema
+    schema = yamale.make_schema('tests/test_cases/Preprocessing/raspi_deployment_config_schema.yml')
+
+    # Get configuration file
+    config_file  = yamale.make_data('src/raspi_deployment_config.yml')
+
+    # Validate data against the schema. Throws a ValueError if data is invalid.
+    raised = True
+    try:
+        _ = yamale.validate(schema, config_file)
+        raised = False
+    finally:
+        assert raised != True
