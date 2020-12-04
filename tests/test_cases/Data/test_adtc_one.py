89922af8d1b428b665872e6ed835465f9db36ff1
Sadeghi <r.sadeghi@tue.nl>
2020-12-04 13:35:26 +0100

    add file size check

--- tests/test_cases/Data/test_adtc_one.py
+++ tests/test_cases/Data/test_adtc_one.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 
 def test_sequential_data_existence():
-    data_directory="prod_data\\sequential\\test"
+    data_directory="prod_data/sequential/test"
     test_mfcc_data = os.path.join(data_directory, "mfcc_test.joblib")
     test_labels_data = os.path.join(data_directory, "labels_test.joblib")
     data_exist = True
@@ -30,5 +30,27 @@ def test_siamese_data_existence():
     if not os.path.exists(test_mfcc_data) or not os.path.exists(test_labels_data) or not os.path.exists(test_lmfe_data):
         data_exist = False
     assert data_exist
+
+
+def test_sequential_file_size():
+    file_size_threshold = 2000 #2K
+    data_directory="prod_data/sequential/test"
+    test_mfcc_data = os.path.join(data_directory, "mfcc_test.joblib")
+    test_labels_data = os.path.join(data_directory, "labels_test.joblib")
+    mfcc_size = os.path.getsize(test_mfcc_data)
+    labels_size = os.path.getsize(test_labels_data)
+    assert labels_size > file_size_threshold and mfcc_size > file_size_threshold
+
+
+def test_siamese_file_size():
+    file_size_threshold = 2000 #2K
+    data_directory="prod_data/siamese/test"
+    test_mfcc_data = os.path.join(data_directory, "mfcc_test.joblib")
+    test_labels_data = os.path.join(data_directory, "labels_test.joblib")
+    test_lmfe_data = os.path.join(data_directory, "lmfe_test.joblib")
+    mfcc_size = os.path.getsize(test_mfcc_data)
+    lmfe_size = os.path.getsize(test_lmfe_data)
+    labels_size = os.path.getsize(test_labels_data)
+    assert labels_size > file_size_threshold and lmfe_size > file_size_threshold and mfcc_size > file_size_threshold
 
 
